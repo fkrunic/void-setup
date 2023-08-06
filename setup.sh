f10497b4a72f60898f0f2e1395f967bc9630ad42
Filip Krunic <fkrunic9@gmail.com>
2023-08-05 21:01:44 -0700

    added firefox / softlink forces override

--- setup.sh
+++ setup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Install window manager
+# Setup window manager
 echo "-------------------------------- Installing Dependencies -------------------------------"
 sudo xbps-install xorg i3 dmenu
 
+# Install browser
+sudo xbps-install firefox
+
 # Port-over config files
 echo "-------------------------------- Porting Config Files -------------------------------"
-ln -s .xinitrc ~/.xinitrc
+ln -sf .xinitrc ~/.xinitrc
 
 echo "-------------------------------- Done ----------------------------------"
 
