99b5f17f4d999fc566a29f58015e9a67f631ba5d
Filip Krunic <fkrunic9@gmail.com>
2023-08-05 20:59:52 -0700

    added section messages

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # Install window manager
+echo "-------------------------------- Installing Dependencies -------------------------------"
 sudo xbps-install xorg i3 dmenu
 
 # Port-over config files
+echo "-------------------------------- Porting Config Files -------------------------------"
 ln -s .xinitrc ~/.xinitrc
+
+echo "-------------------------------- Done ----------------------------------"
+
